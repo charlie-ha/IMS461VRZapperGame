405c350c96fc82287e0572d91d79418bb2819873
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2025-03-10 23:09:30 -0400

    target prefabs and spawner

---
+++ Assets/Script/TargetSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public int targetSpawnAmount = 5;
+    [SerializeField] private GameObject targetPrefabs;
+    private float spawnTimer = 10;//every 10s
+    private float resetTimer = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resetTimer += 1 * Time.deltaTime;
+            if(resetTimer >= spawnTimer)
+            {
+                //canSpawn = true;
+                resetTimer = 0;
+            }
+    }
+
+    void SpawnTargets()
+    {
+        for(int i = 0; i < targetSpawnAmount; i++)
+        {
+            int spawnRandom = Random.Range(0, spawnPoints.Length);//random positions near spawn points
+            Instantiate(targetPrefabs, new Vector3(spawnPoints[spawnRandom].position.x - Random.Range(0, 10), spawnPoints[spawnRandom].position.y, spawnPoints[spawnRandom].position.z - Random.Range(0, 5)), spawnPoints[i].rotation);//which points to spawn; positions; rotation
+        }
+    }
+}
