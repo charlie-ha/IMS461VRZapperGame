fdd1bcd0e5b19a287672762b596f9e750abb7263
WWiLD-FiRE <17tannerman@gmail.com>
2025-04-16 15:11:58 -0400

    code for tp and gun
    button for tp and gun spawning (needs tested)

---
+++ Assets/Script/NewGunSpawn.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class NewGunSpawn : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gunPrefab;
+    [SerializeField] private Transform gunSpawnPoint;
+    [SerializeField] public GameObject player;
+    
+    public UnityEvent onPressGun;
+    public UnityEvent onReleaseGun;
+    GameObject presser;
+
+    bool isGrabbed;
+
+
+
+    void Start()
+    {
+
+        isGrabbed = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isGrabbed)
+        {
+            
+            presser = other.gameObject;
+            onPressGun.Invoke();
+
+            isGrabbed = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == presser)
+        {
+          
+            onReleaseGun.Invoke();
+            isGrabbed = false;
+            
+        }
+    }
+
+    public void SpawnNewGun()
+    {
+        Instantiate(gunPrefab, gunSpawnPoint.position, gunSpawnPoint.rotation);
+    }
+
+}
+    
