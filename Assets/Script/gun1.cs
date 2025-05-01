a7aa4e4f32730adbcd00fd1a5083067491ee42f0
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2025-04-30 20:53:48 -0400

    d

--- Assets/Script/gun1.cs
+++ Assets/Script/gun1.cs
@@ -130,7 +130,7 @@ public class gun1 : MonoBehaviour
                 //Debug.Log("We've shot" + hitInfo.point);
                 //laserLine.SetPosition(1, hitInfo.point);
                 GameObject hitObject = hitInfo.collider.gameObject;
-                Debug.Log(hitObject.name);
+                //Debug.Log(hitObject.name);
                 // if (hitObject.name == "intact_target" || hitObject.layer == LayerMask.NameToLayer("target"))
                 // {
                 //     // Spawn broken target at the same position and rotation
