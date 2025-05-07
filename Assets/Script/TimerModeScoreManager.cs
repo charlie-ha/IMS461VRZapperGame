300f612f560e80fb90754b273bf10902101c0b8f
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2025-05-06 23:05:45 -0400

    d

---
+++ Assets/Script/TimerModeScoreManager.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerModeScoreManager : MonoBehaviour
+{
+    public int TM_score = 0;//TM_ stands for Timer Mode
+    public int TM_redTargetBroken = 0;
+    public int TM_blueTargetBroken = 0;
+    [SerializeField] private TMP_Text scoreText; // Drag your score UI TextMeshPro object here
+    [SerializeField] private Text redTargetText;
+    [SerializeField] private Text blueTargetText;
+
+    public void AddScore(string targetTag)
+    {
+        if (targetTag == "TM_redTarget")
+        {
+            TM_score += 5;
+            TM_redTargetBroken += 1;
+        }
+        else if (targetTag == "TM_blueTarget")
+        {
+            TM_score += 10;
+            TM_blueTargetBroken += 1;
+        }
+        else if (targetTag == "TM_movingBlueTarget")
+        {
+            TM_score += 15;
+            TM_blueTargetBroken += 1;
+        }
+        else if (targetTag == "TM_movingRedTarget")
+        {
+            TM_score += 7;
+            TM_blueTargetBroken += 1;
+        }
+
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = TM_score.ToString();
+            redTargetText.text = "- Red Targets Broken - " + TM_redTargetBroken.ToString();
+            blueTargetText.text = "- Blue Targets Broken - " + TM_blueTargetBroken.ToString();
+        }
+    }
+}
